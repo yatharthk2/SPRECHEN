9a1df6694487fbcb96ea3178bcac72e20b878412
yatharthk2 <yatharth.kapadia.1702@gmail.com>
2021-08-20 18:42:44 +0530

    init

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
-print('hello world')
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import spacy
+from utils import translate_sentence, bleu, save_checkpoint, load_checkpoint
+from torch.utils.tensorboard import SummaryWriter
+from torchtext.datasets import Multi30k
+from torchtext.data import Field, BucketIterator
