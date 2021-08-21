c00486fe8adc687c3bce02637f5e8ac2a316182e
yatharthk2 <yatharth.kapadia.1702@gmail.com>
2021-08-21 21:48:56 +0530

    removed error

--- utils.py
+++ utils.py
@@ -68,4 +68,3 @@ def load_checkpoint(checkpoint, model, optimizer):
     print("=> Loading checkpoint")
     model.load_state_dict(checkpoint["state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer"])
-© 2021 GitHub, Inc.
